593989282b303823f898065333de8aa3a902957c
PaolaMateos <paomateos@gmail.com>
2015-09-19 22:10:18 -0500

    cc

---
+++ boxMuller/server.R
@@ -0,0 +1,36 @@
+#Tarea 2 Box Muller
+#Método de generación de pares de números aleatorios indep, con distribución
+#Normal estándar
+
+#Generamos dos v.a.i. uniformemente distribuidas en (0,1] y utilizamos el 
+#método polar para transformar las variables
+library(shiny)
+
+#u1 <- runif(1)
+#u2 <- runif(1)
+
+x <- function(...) {
+  u1 <- runif(1)
+  u2 <- runif(1)
+  sqrt(-2*log(u2))*cos(2*pi*u1)}
+
+y <- function(...){
+  u3 <- runif(1)
+  u4 <- runif(1)
+  sqrt(-2*log(u4))*sin(2*pi*u3)
+    }
+
+shinyServer(function(input, output) {
+  
+  # Expression that generates a histogram. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+ 
+  output$distPlot <- renderPlot({
+    xg <- sapply(numeric(input$X),x)
+    yg <-sapply(numeric(input$Y),y)
+    # draw the histogram with the specified number of bins
+    hist(yg, col = 'skyblue', border = 'white')
+    
+  })
+})
---
+++ boxMuller/ui.R
@@ -0,0 +1,34 @@
+library(shiny)
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Box Muller"),
+  
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("X",
+                  "X:",
+                  min = 1,
+                  max = 4000,
+                  value = 30),
+      
+      
+      sliderInput("Y",
+                  "Y:",
+                  min = 1,
+                  max = 4000,
+                  value = 30)
+      
+      ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      tabsetPanel(
+        tabPanel("X",plotOutput("distPlot")),
+        tabPanel("Y",plotOutput("yplot"))
+      #plotOutput("distPlot")
+    ))
+  )
+))
